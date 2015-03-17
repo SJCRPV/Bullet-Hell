38f055501e56c0baefd89a54a97c566b622b356f
SJCRPV <sjcrpv.games@hotmail.com>
2015-03-17 19:46:31 +0000

    Test
    Simply testing if the project loaded

--- Bullet-Hell/Assets/Scripts/LevelDatabase.cs
+++ Bullet-Hell/Assets/Scripts/LevelDatabase.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class LevelDatabase : MonoBehaviour {
 
-	private int[] levelArray = new int[
+	private int[] levelArray = new int[5];
 
+
+	public int[] Level1()
+	{
+		levelArray[0] = 10;
+		levelArray[1] = 12;
+		levelArray[2] = 7;
+		levelArray[3] = 12;
+		levelArray[4] = 1;
+	}
 
 	// Use this for initialization
 	void Start () {
