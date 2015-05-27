c1ce615a174f5a9b7aadbb3a606272517c30be54
SJCRPV <sjcrpv.games@hotmail.com>
2015-05-27 13:54:50 +0100

    Database Restructure
    Restructured how the Level Database works. Now it can receive the enemy
    spawn blueprints at level load. You don't need to define it at spawn
    time.

--- Bullet-Hell/Assets/Scripts/LevelDatabase.cs
+++ Bullet-Hell/Assets/Scripts/LevelDatabase.cs
@@ -10,26 +10,71 @@ public class LevelDatabase : MonoBehaviour {
 	public GameObject enemyCone;
 	public GameObject enemyBoss1;
 
-	public int[] levelArray = new int[5];
-
-	void OnLevelWasLoaded(int level)
+	public enum enemyList
 	{
-		switch(level)
+		//Change the names when you figure out what to call them
+		Basic,
+		Cone,
+		Alternate,
+		Enemy4,
+		Enemy5,
+		Enemy6,
+		Enemy7,
+		Enemy8,
+		Enemy9,
+		Enemy10,
+		Enemy11,
+		Boss1,
+		Boss2,
+		Boss3,
+		Boss4,
+		Boss5,
+		Boss6,
+		Boss7,
+		Boss8,
+	}
+
+	public int[] levelArray = new int[5,15];
+
+	void fillArray()
+	{
+		switch(currentLevel)
 		{
-		case 0:
-			Level0();
-		break;
-
 		case 1:
-			Level1();
+			for(int j = 0; j < 5; j++)
+			{
+				switch(j)
+				{
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+					for(int i = 0; i < 15; i++)
+					{
+						//When you figure out which enemies show up in which levels, re-do this
+						if(i > levelArray[0,0])
+						{
+							levelArray[0, i] = null;
+						}
+						else if(i % 4 != 0)
+						{
+							levelArray[0, i] = enemyList.Basic;
+						}
+						else
+						{
+							levelArray[0, i] = enemyList.Cone;
+						}
+					}
+					break;
+
+				case 4:
+					levelArray[0, 1] = enemyList.Boss1;
+					break;
+				}
+			}
 		break;
 
-		case 2:
-			Level2();
-		break;
 
-		default:
-		break;
 		}
 	}
 
@@ -42,16 +87,38 @@ public class LevelDatabase : MonoBehaviour {
 	{
 		Debug.Log("You loaded level 1!");
 		currentLevel = 1;
-		levelArray[0] = 8;
-		levelArray[1] = 10;
-		levelArray[2] = 10;
-		levelArray[3] = 5;
-		levelArray[4] = 1;
+		levelArray[0,0] = 8;
+		levelArray[1,0] = 10;
+		levelArray[2,0] = 10;
+		levelArray[3,0] = 15;
+		levelArray[4,0] = 1;
+		fillArray();
 	}
 	public void Level2()
 	{
 		Debug.Log("You loaded level 2!");
 		currentLevel = 2;
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		switch(level)
+		{
+		case 0:
+			Level0();
+			break;
+			
+		case 1:
+			Level1();
+			break;
+			
+		case 2:
+			Level2();
+			break;
+			
+		default:
+			break;
+		}
 	}
 
 	// Use this for initialization
--- Bullet-Hell/Assets/Scripts/SpawnEnemies.cs
+++ Bullet-Hell/Assets/Scripts/SpawnEnemies.cs
@@ -11,6 +11,9 @@ public class SpawnEnemies : MonoBehaviour {
 	private int currentLevel;
 	private int phaseTotal;
 	private float newPhaseTimerStore;
+	private Vector3 endPosition;
+	private Vector3 startPosition;
+	private int phaseTotal;
 
 	/*Increments with enemy spawns.Resets at half-phase.Determines how much you add to endPos
 Look for a better name*/
@@ -34,41 +37,100 @@ Look for a better name*/
 		return addOn;
 	}
 
-	void spawnEnemy()
+	void spawnEnemy(int objectToSpawn)
 	{
-		if(gameObject.tag == "Basic")
+		if(objectToSpawn.tag == "Basic")
+		//if(gameObject.tag == "Basic")
 		{
 			//Debug.Log("Spawned a basic!");
-			enemyInstance = (GameObject)Instantiate(levelDatabaseScript.enemyBasic, transform.position, Quaternion.identity);
-			enemyInstance.name = "Enemy01";
+			enemyInstance = (GameObject)Instantiate(levelDatabaseScript.enemyBasic, startPosition, Quaternion.identity);
+			enemyInstance.name = "Basic";
 		}
-		else if(gameObject.tag == "Cone")
+		else if(objectToSpawn.tag == "Cone")
+		//else if(gameObject.tag == "Cone")
 		{
 			//Debug.Log ("Spawned a cone!");
-			enemyInstance = (GameObject)Instantiate(levelDatabaseScript.enemyCone, transform.position, Quaternion.identity);
-			enemyInstance.name = "Enemy02";
+			enemyInstance = (GameObject)Instantiate(levelDatabaseScript.enemyCone, startPosition, Quaternion.identity);
+			enemyInstance.name = "Cone";
+		}
+	}
+
+	void moveToNextPhase()
+	{
+		if(levelDatabaseScript.currentLevelPhase < levelDatabaseScript.levelArray.Length)
+		{
+			levelDatabaseScript.currentLevelPhase++;
+			phaseTotal = levelDatabaseScript.levelArray[levelDatabaseScript.currentLevelPhase];
+		}
+
+		newPhaseTimer = newPhaseTimerStore;
+	}
+
+	void spawnPattern()
+	{
+		for(int i = 0; i < phaseTotal; i++)
+		{
+			if(startPosition == this.transform && i < phaseTotal/2 - 1)
+			{
+				spawnEnemy (0);
+			}
+			else if(startPosition == this.transform && i == phaseTotal/2 - 1)
+			{
+				spawnEnemy(levelDatabaseScript.enemyCone);
+			}
+			else if(
 		}
 	}
 
 	void setDestination()
 	{
-		if(endPosAdjustment < levelDatabaseScript.levelArray[levelDatabaseScript.currentLevelPhase]/2)
+		if(startPosition == this.transform)
 		{
+			endPosition = GameObject.Find("EnemyEndPoint1").transform + adjustmentToEndPosition();
+			Debug.Log(gameObject.name + " which is number " + endPosAdjustment +  " is moving to: " + endPosition);
+		}
+		else if(startPosition == GameObject.Find("EnemySpawnPoint2").transform)
+		{
+			endPosition = GameObject.Find("EnemyEndPoint2").transform - adjustmentToEndPosition();
+			Debug.Log(gameObject.name + " which is number " + endPosAdjustment +  " is moving to: " + endPosition);
+		}
+		else
+		{
+			Debug.LogError(gameObject.name + " which is number " + endPosAdjustment + " does not have a destination");
+		}
+	}
 
+	void setStartingPoint()
+	{
+		for(int i = 0; i < phaseTotal; i++)
+		{
+			if(i < phaseTotal/2)
+			{
+				startPosition = this.transform;
+			}
+			else
+			{
+				startPosition = GameObject.Find("EnemySpawnPoint2").transform;
+			}
 		}
 	}
 
 	// Use this for initialization
 	void Start () {
 		levelDatabaseScript = GetComponent<LevelDatabase>();
-		spawnBossScript = GetComponent<SpawnBoss> ();
+		spawnBossScript = GetComponent<SpawnBoss>();
 		newPhaseTimerStore = newPhaseTimer;
+		setStartingPoint();
 		setDestination();
-		spawnEnemy();
+		spawnPattern();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		newPhaseTimer -= Time.deltaTime;
+		if(newPhaseTimer <= 0)
+		{
+			moveToNextPhase();
+		}
 	}
 }
